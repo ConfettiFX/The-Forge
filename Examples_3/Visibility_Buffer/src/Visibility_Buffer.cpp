64325a45b8a74ae380ed8f2a7e7180bdc5999092
manaskulkarni786 <manas@conffx.com>
2018-04-05 21:54:22 +0530

    Visibility Buffer - Light cluster bugfix

--- Examples_3/Visibility_Buffer/src/Visibility_Buffer.cpp
+++ Examples_3/Visibility_Buffer/src/Visibility_Buffer.cpp
@@ -1584,7 +1584,7 @@ public:
 			if (!gAppSettings.mAsyncCompute || !gAppSettings.mFilterTriangles)
 			{
 				cmdBeginGpuTimestampQuery(graphicsCmd, pGraphicsGpuProfiler, "Clear Light Clusters", true);
-				clearLightClusters(graphicsCmd, (graphicsFrameIdx + 1) % gImageCount);
+				clearLightClusters(graphicsCmd, (graphicsFrameIdx) % gImageCount);
 				cmdEndGpuTimestampQuery(graphicsCmd, pGraphicsGpuProfiler);
 			}
 
@@ -1592,8 +1592,9 @@ public:
 			{
 				// Update Light clusters on the GPU
 				cmdBeginGpuTimestampQuery(graphicsCmd, pGraphicsGpuProfiler, "Compute Light Clusters", true);
-				cmdSynchronizeResources(graphicsCmd, 1, &pLightClustersCount[(graphicsFrameIdx + 1) % gImageCount], 0, NULL, false);
-				computeLightClusters(graphicsCmd, (graphicsFrameIdx + 1) % gImageCount);
+				cmdSynchronizeResources(graphicsCmd, 1, &pLightClustersCount[(graphicsFrameIdx) % gImageCount], 0, NULL, false);
+				computeLightClusters(graphicsCmd, (graphicsFrameIdx) % gImageCount);
+				cmdSynchronizeResources(graphicsCmd, 1, &pLightClusters[(graphicsFrameIdx) % gImageCount], 0, NULL, false);
 				cmdEndGpuTimestampQuery(graphicsCmd, pGraphicsGpuProfiler);
 			}
 
